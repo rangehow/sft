1002306c1c96a4a27ea6f241cf290ed61fd38a47
rangehow <a954369920@gmail.com>
2024-05-13 15:49:47 +0800

    torchrun

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=1
-python special_train.py --model gemma_2b --dataset alpaca_gpt4 --output_dir ../output --fa2
+export CUDA_VISIBLE_DEVICES=1,2,3
+torchrun --nproc-per-node 3 special_train.py --model gemma_2b --dataset alpaca_gpt4 --output_dir ../output
