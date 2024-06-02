27c22b328f3ff0ee52e6bb3ee1e0992792657103
rangehow <a954369920@gmail.com>
2024-06-02 11:31:40 +0800

    去掉gsm8k里的测试打印

--- eval.sh
+++ eval.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=2,3 python -m sft.eval.gsm8k --dataset gsm8k --model /data/
 
 CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python -m sft.eval.gsm8k --dataset mmlu --model /data/ruanjh/best_training_method/gemma-2b  --vllm --mode 0 --shot --data_parallel
 
-CUDA_VISIBLE_DEVICES=2 python -m sft.eval.gsm8k --model models/Meta-Llama-3-8B-Instruct --dataset mmlu --vllm --mode 0 --shot &
+ python -m sft.eval.gsm8k --model models/Meta-Llama-3-8B-Instruct --dataset mmlu --vllm --mode 0 --shot --dp &
 
 CUDA_VISIBLE_DEVICES=2 python -m sft.eval.gsm8k --model sft/gemma_2b_alpaca_gpt4_5m30d_0_weighted_div  --dataset gsm8k --vllm --mode 0 --shot &
 
