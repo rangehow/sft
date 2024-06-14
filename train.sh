ebf869dbeb2f1984a67f32c2d2468891486fce94
rangehow <a954369920@gmail.com>
2024-06-14 23:11:44 +0800

    .

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=1 python /data/ruanjh/best_training_method/sft/special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.8
+CUDA_VISIBLE_DEVICES=1 python special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.8
 
-CUDA_VISIBLE_DEVICES=2 python /data/ruanjh/best_training_method/sft/special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 1
+CUDA_VISIBLE_DEVICES=2 python special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 1
 
-CUDA_VISIBLE_DEVICES=3 python /data/ruanjh/best_training_method/sft/special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.5
+CUDA_VISIBLE_DEVICES=3 python special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.5
