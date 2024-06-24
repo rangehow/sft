a0d8529639995af35b06e1987cfdb5919c59d7aa
rangehow <a954369920@gmail.com>
2024-06-24 22:49:39 +0800

    .

--- train.sh
+++ train.sh
@@ -14,6 +14,8 @@ CUDA_VISIBLE_DEVICES=2 python special_train.py --gradient_accumulation_steps 32 
 
 
 CUDA_VISIBLE_DEVICES=1 python special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.5 --div_mode False --weighted True&
-CUDA_VISIBLE_DEVICES=2 python special_train.py --gradient_accumulation_steps 64 --total_bsz 256 --zero_prob 0 --alpha 0.7 --div_mode False --weighted True --dataset alpaca_gpt4,code,math&
-CUDA_VISIBLE_DEVICES=3 python special_train.py --gradient_accumulation_steps 64 --total_bsz 256 --zero_prob 0 --alpha 0.3 --div_mode False --weighted True --dataset alpaca_gpt4,code,math&
+CUDA_VISIBLE_DEVICES=2 python special_train.py --gradient_accumulation_steps 512 --total_bsz 512 --zero_prob 0 --alpha 0.5 --div_mode True --weighted True --dataset alpaca_gpt4,code,math&
+CUDA_VISIBLE_DEVICES=3 python special_train.py --gradient_accumulation_steps 512 --total_bsz 512 --zero_prob 0 --alpha 0.5 --div_mode True --weighted False --dataset alpaca_gpt4,code,math&
+
+CUDA_VISIBLE_DEVICES=1 python special_train.py --gradient_accumulation_steps 512 --total_bsz 512 --zero_prob 0 --div_mode True --weighted True --dataset alpaca_gpt4,code,math --mix True --mix_ratio 0.5&
 # CUDA_VISIBLE_DEVICES=3 python special_train.py --gradient_accumulation_steps 32 --total_bsz 256 --zero_prob 0 --alpha 0.8 --div_mode False &
