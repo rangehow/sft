5c0a4eda984244394d9a0d68bee54fd358466cdf
rangehow <a954369920@gmail.com>
2024-07-15 20:38:15 +0800

    .

--- special_train.py
+++ special_train.py
@@ -234,7 +234,7 @@ logger.debug(args)
 # )
 real_bsz = args.total_bsz // args.gradient_accumulation_steps
 logger.debug(
-    f"实际的总batch_size=梯度累计{args.gradient_accumulation_steps}x每张卡的bsz{real_bsz}x卡的数量{torch.cuda.device_count()}={args.gradient_accumulation_steps*real_bsz*torch.cuda.device_count()}"
+    f"实际的总batch_size=梯度累计{args.gradient_accumulation_steps}x每张卡的bsz{real_bsz}={args.gradient_accumulation_steps*real_bsz}"
 )
 
 if args.lora:
