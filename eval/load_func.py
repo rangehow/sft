b9911a0c4e1eeb021f4b5251848af2f2f1341fcd
rangehow <a954369920@gmail.com>
2024-07-21 13:22:49 +0800

    修改了评测输出的方式，从time/model到model/time，很可能有bug

--- eval/load_func.py
+++ eval/load_func.py
@@ -4,66 +4,62 @@ import datasets
 dname2load = {}
 
 
-def register2dict():
+def register2dict(name):
     def decorator(func):
-        if func.__name__ not in dname2load:
-            dname2load[func.__name__] = func
-        else:
-            exit()
+        dname2load[name] = func
         return func
 
     return decorator
 
 
-@register2dict()
+@register2dict(name="gsm8k")
 def gsm8k(local_dir):
     if local_dir is not None:
         return datasets.load_dataset(local_dir, "main")["test"]
-
+    
     return datasets.load_dataset("gsm8k", "main")["test"]
 
 
-@register2dict()
+@register2dict(name="bbh")
 def bbh(local_dir):
     if local_dir is not None:
         return datasets.load_dataset(local_dir)["train"]
-
+    
     return datasets.load_dataset("JesusCrist/bbh_cot_fewshot")["train"]
 
 
-@register2dict()
+@register2dict(name="code")
 def code(local_dir):
     if local_dir is not None:
         return datasets.load_dataset(local_dir)["train"]
     return datasets.load_dataset("m-a-p/CodeFeedback-Filtered-Instruction")["train"]
 
-
-@register2dict()
+@register2dict(name="apps")
 def apps(local_dir):
-    exit()  # '***数据集，不许用'
+    exit() # '***数据集，不许用'
     if local_dir is not None:
         return datasets.load_dataset(local_dir, "all")["train"]
     return datasets.load_dataset("codeparrot/apps", "all")["train"]
 
-
-@register2dict()
+@register2dict(name="alpaca_gpt4")
 def alpaca_gpt4(local_dir):
     if local_dir is not None:
         return datasets.load_dataset(local_dir)["train"]
     return datasets.load_dataset("vicgalle/alpaca-gpt4")["train"]
 
 
-@register2dict()
+
+@register2dict(name="mmlu")
 def mmlu(local_dir):
     if local_dir is not None:
         return datasets.load_dataset(local_dir, "all")["test"]
     return datasets.load_dataset("cais/mmlu", "all")["test"]
 
 
-@register2dict()
+@register2dict(name="humaneval")
 def humaneval(local_dir):
     if local_dir is not None:
-        return datasets.load_dataset(local_dir)["test"]
+        return datasets.load_dataset(local_dir)['test']
     # from human_eval.data import write_jsonl, read_problems
     # problems = read_problems()
 
@@ -75,130 +71,25 @@ def humaneval(local_dir):
     #     for task_id in problems
     #     for _ in range(num_samples_per_task)
     # ]
-    return datasets.load_dataset("openai/openai_humaneval")["test"]
-
-
-@register2dict()
+    return datasets.load_dataset('openai/openai_humaneval')['test']
+    
+@register2dict(name="truthfulqa")
 def truthfulqa(local_dir):
     if local_dir is not None:
-        return datasets.load_dataset(local_dir, "multiple_choice")["validation"]
+        return datasets.load_dataset(local_dir, "multiple_choice")['validation']
     else:
-        return datasets.load_dataset("truthful_qa", "multiple_choice")["validation"]
+        return datasets.load_dataset('truthful_qa', "multiple_choice")['validation']
 
-
-@register2dict()
+@register2dict(name="math")
 def math(local_dir):
     if local_dir is not None:
-        a = datasets.load_dataset(local_dir, "all")["train"]
-        b = datasets.load_dataset(local_dir, "all")["test"]
+        a= datasets.load_dataset(local_dir,'default')['train']
+        b=datasets.load_dataset(local_dir,'default')['test']
     else:
-        a = datasets.load_dataset("lighteval/MATH", "all")["train"]  # 7.5k
-        b = datasets.load_dataset("lighteval/MATH", "all")["test"]  # 5k
-    return datasets.concatenate_datasets([a, b])
-
-
-@register2dict()
-def magpie(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("Magpie-Align/Magpie-Pro-300K-Filtered")["train"]
-
-
-@register2dict()
-def redpajama(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("togethercomputer/RedPajama-Data-1T-Sample")[
-            "train"
-        ]
-
-
-@register2dict()
-def slimpajama(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("DKYoon/SlimPajama-6B")["train"]
-
-
-@register2dict()
-def test(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("JesusCrist/SimpleData")["train"]
-
-
-@register2dict()
-def wiki_medical(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("gamino/wiki_medical_terms")["train"]
-
-
-@register2dict()
-def medical_transcription(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("adisingh19/medical_transcriptions")["train"]
-
-
-@register2dict()
-def textbooks(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("MedRAG/textbooks")["train"]
-
-
-@register2dict()
-def medpt(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("FreedomIntelligence/huatuo_encyclopedia_qa")[
-            "train"
-        ]
-
-
-@register2dict()
-def medqa(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("bigbio/med_qa")["train"]
-
-
-@register2dict()
-def medquad(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("keivalya/MedQuad-MedicalQnADataset")["train"]
-
-
-@register2dict()
-def medmcqa(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["test"]
-    else:
-        return datasets.load_dataset("openlifescienceai/medmcqa")["test"]
-
-
-@register2dict()
-def medical(local_dir):
-    if local_dir is not None:
-        return datasets.load_dataset(local_dir)["train"]
-    else:
-        return datasets.load_dataset("JesusCrist/med_qa_fewshot")["train"]
-
+        a=datasets.load_dataset('lighteval/MATH','default')['train'] # 7.5k
+        b=datasets.load_dataset('lighteval/MATH','default')['test']  # 5k
+    return datasets.concatenate_datasets([a,b])
 
 if __name__ == "__main__":
-    d = datasets.load_dataset("medpt")
-    import pdb
+    humaneval()
 
-    pdb.set_trace()
