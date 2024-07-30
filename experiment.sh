3124be74392646bf382ee0d8accea08e8f551ac8
rangehow <a954369920@gmail.com>
2024-07-30 10:38:43 +0800

    .

--- experiment.sh
+++ experiment.sh
@@ -83,3 +83,6 @@ python naive_train.py --model llama3_8b --gradient_accumulation_steps 256 --tota
 
 
 python special_train.py --model qwen_med_pt --gradient_accumulation_steps 128 --total_bsz 256  --dataset medquad --w_template True --num_train_epochs 3 --learning_rate 2e-5 --lr_scheduler_type cosine --warmup_ratio 0.05 --mono True --mono_dataset textbooks --mix True --mix_ratio 0.8 --div_mode False --weighted False --template qwen2
+
+
+python naive_train.py --model qwen2_7b --gradient_accumulation_steps 128 --total_bsz 256  --dataset pubmed_abstrace  --w_template False --num_train_epochs 1 --learning_rate 3e-4 --lr_scheduler_type cosine --warmup_ratio 0.05
