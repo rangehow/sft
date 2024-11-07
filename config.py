6023c8858bd4ef81e503fbc568b374426c35a4a1
rangehow <a954369920@gmail.com>
2024-11-07 14:06:43 +0800

    .

---
+++ config.py
@@ -0,0 +1,11 @@
+model_dir = {
+    "qwen2.5-7b": "/mnt/rangehow/models/Qwen2.5-7B-Instruct",
+    "gemma-2b": "/niutrans/NEUNLP/rjh/models/gemma-2b",
+    "gemma2-27b-it": "/niutrans/NEUNLP/rjh/models/gemma-2-27b-it",
+}
+
+dataset_dir = {
+    "alpaca_gpt4": "vicgalle/alpaca-gpt4",
+    "code": "m-a-p/CodeFeedback-Filtered-Instruction",
+    "math": "lighteval/MATH",
+}
